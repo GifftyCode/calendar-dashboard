695554f5b0e401d82435a13691280a8fbda99222
GifftyCode <ulokangozi@yahoo.com>
2025-06-12 13:47:31 +0100

    updated code

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "react/jsx-runtime": "react/jsx-runtime.js",
-      "react/jsx-dev-runtime": "react/jsx-dev-runtime.js",
-    },
+  esbuild: {
+    jsx: "automatic",
   },
   optimizeDeps: {
     include: ["react", "react-dom"],
